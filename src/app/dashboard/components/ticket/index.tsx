b33175002efaa942a5f90ce8fadc00e4077a24aa
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-01-29 12:51:14 -0300

    listando todos os tickets na página dashboard

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,17 +1,24 @@
+import { CustomerProps } from '@/utils/customers.type';
+import { TicketProps } from '@/utils/ticket.type';
 import { FiTrash2, FiFile} from 'react-icons/fi';
 
-export function TicketItem() {
+interface TicketItemProps {
+    ticket: TicketProps;
+    customer: CustomerProps | null;
+}
+
+export function TicketItem({ customer, ticket }: TicketItemProps) {
     return(
         <>
             <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
                 <td className="text-left pl-1">
-                    Mercado Silva
+                    {customer?.name}
                 </td>
                 <td className="text-left hidden sm:table-cell">
-                    17/01/2024
+                    {ticket.created_at?.toLocaleDateString("pt-br")}
                 </td>
                 <td className="text-left">
-                    <span className="bg-green-500 px-2 py-1 rounded">ABERTO</span>
+                    <span className="bg-green-500 px-2 py-1 rounded">{ticket.status}</span>
                 </td>
                 <td className="text-left">
                     <button className="mr-2">
